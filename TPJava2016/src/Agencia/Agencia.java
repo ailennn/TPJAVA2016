32de1fb439da2df8161aed5ecc1f8673f33961e4
florenciaidone <florenciaidone_9@hotmail.com>
2016-04-18 21:08:45 -0300

    baaa

--- TPJava2016/src/Agencia/Agencia.java
+++ TPJava2016/src/Agencia/Agencia.java
@@ -8,16 +8,10 @@ public class Agencia {
 /**Contiene las listas de transportes, destinos, y resposables
  * 	para poder armar los viajes, hacer los ABM de destino, viajes terminados, responsables
  */
-<<<<<<< HEAD
-	//LinkedList Transporte
-	//LinkedList Responsables
-	//
-
-=======
 
 	private LinkedList<Transporte> listaTrasporte;
 	private LinkedList<Destino> listaDestino;
 	
->>>>>>> ce425d6d14f3ac5af39b606ab69f607dd9a9d0ee
+
 
 }
