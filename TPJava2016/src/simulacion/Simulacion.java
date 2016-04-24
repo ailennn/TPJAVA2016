b82fe58e1cd4f2339599de3977c0262abdbd9e76
florenciaidone <florenciaidone_9@hotmail.com>
2016-04-24 19:56:57 -0300

    s

--- TPJava2016/src/simulacion/Simulacion.java
+++ TPJava2016/src/simulacion/Simulacion.java
@@ -8,32 +8,30 @@ public class Simulacion implements Runnable {
 	private Viaje viaje;
 	private Agencia agencia;
 	private long initialTime;
+	private static boolean pausa=false;
 	
-	/* chicas:
-	 * Descomentar el public static void main(String[] args) { y el agencia.setListaViajesPendientes();
-	 * y comentar el public IniciarSimulacion (Viaje viaje, Agencia agencia, long initialTime){
-	 * si quieren ver como funciona la simulacion
-	 */
-	//public static void main(String[] args) {
 	public Simulacion (Viaje viaje, Agencia agencia, long initialTime){
 		this.agencia = agencia;
 		this.viaje = viaje;
 		this.initialTime = initialTime;
 	}
 	
-	public static void inicio() {
+	public static void inicio(Viaje v, Agencia agencia, long initialTime) {
 		// TODO Auto-generated method stub
-		Agencia agencia = new Agencia();
 		//agencia.setListaViajesPendientes();
 		ListIterator<Viaje> iterador=agencia.getListaViajesPendientes().listIterator();
-		Viaje v=null;
 		// Tiempo inicial de referencia
-		long initialTime = System.currentTimeMillis();
+		initialTime = System.currentTimeMillis();
 		
 		while(iterador.hasNext()){
 			v=iterador.next();
 			Runnable proceso = new Simulacion(v,agencia,initialTime);
-			new Thread(proceso).start();
+			if(pausa==false)
+				new Thread(proceso).start();
+			else{
+				new Thread(proceso).notifyAll();
+				pausa=false;
+			}
 		}
 
 	}
@@ -43,20 +41,26 @@ public class Simulacion implements Runnable {
 		this.agencia.iniciarViajePendiente(this.viaje,this.initialTime);
 	}
 	
-	public static void detener() {
+	/*
+	public static void detener(Viaje v, Agencia agencia, long initialTime) {
 		// TODO Auto-generated method stub
-		Agencia agencia = new Agencia();
 		//agencia.setListaViajesPendientes();
 		ListIterator<Viaje> iterador=agencia.getListaViajesPendientes().listIterator();
-		Viaje v=null;
 		// Tiempo inicial de referencia
 		long finalTime = System.currentTimeMillis();
 		
 		while(iterador.hasNext()){
 			v=iterador.next();
 			Runnable proceso = new Simulacion(v,agencia,finalTime);
-			new Thread(proceso).interrupt(); 
+			try {
+				new Thread(proceso).wait();
+				pausa=true;
+				agencia.detenerViajePendiente(v,initialTime);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-	}
+	}*/
 
 }
